8eb1c7139bb3a83688a35618100f6530af9f7397
MRfuture17 <31774724+MRfuture17@users.noreply.github.com>
2017-09-24 11:57:11 -0700

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Auto
+{
+    public class Auto
+    {
+        public Auto()
+        { }
+        public string Make { get; }
+        public string Model { get; }
+        public int Year { get; }
+        public string Color { get; }
+
+
+    }
+}
+
